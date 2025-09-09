4efc60024195c8547416d9ab11b7a6eb5b451e67
Fernando Dias <ferpaesdias@gmail.com>
2025-09-09 17:02:37 -0300

    Ajustes

--- network.tf
+++ network.tf
@@ -76,7 +76,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_ranges    = ["2222", "22"]
+    destination_port_ranges    = ["2222"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
